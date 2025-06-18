0251c8f1471fb0d1f2f96249c03c5d101b4bbd12
SanketTops <sanket.tops@gmail.com>
2025-06-18 18:48:26 +0530

    String

---
+++ Assignments/Module-1/string.py
@@ -0,0 +1,31 @@
+mystr="this is python!"
+
+"""print(mystr)
+print(mystr[0]) #first char.
+print(mystr[-1]) #last char.
+print(mystr[0:4]) #string slice - range
+print(len(mystr))"""
+
+# ---------------------------- #
+# String Methods
+print(mystr)
+#print(mystr.strip())
+#print(mystr.lower())
+#print(mystr.upper())
+#print(mystr.replace("This","That"))
+#print(mystr.replace("T",'Y'))
+#print(mystr.split())
+#print(mystr.capitalize())
+#print(mystr.casefold())
+#print(mystr.count("i"))
+#print(mystr.count("This"))
+#print(mystr.startswith("That"))
+#print(mystr.endswith("!"))
+#print(mystr.find("P"))
+#print(mystr.index("PYTHON"))
+#print(mystr.isalpha())
+#print(mystr.isdigit())
+#print(mystr.isalnum())
+#print(mystr.islower())
+#print(mystr.isupper())
+print(mystr.title())
